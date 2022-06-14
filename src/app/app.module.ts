066a6408b48a8568079f04fa9f4d7bf1960bcf11
Taranpreet Singh <taran003@gmail.com>
2022-06-14 20:10:42 +0530

    added project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DepartmentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
